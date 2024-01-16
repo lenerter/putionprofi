bea45b4a9d2b38b85cc1d67d2109b49a7516c59b
lenerter <114988760+lenerter@users.noreply.github.com>
2024-01-16 20:30:32 +0700

    Add files via upload

---
+++ tz2/tz2.py
@@ -0,0 +1,17 @@
+import os
+import pyautogui
+import keyboard
+
+f = open('1.txt', 'r', encoding="utf-8")
+txt = f.readlines()
+if txt == "тревога":
+    
+elif txt == "подозрительно":
+    
+elif:
+    pass
+
+
+
+
+
